e52293bd6e58f2c422b5d77dc86b63a028657946
PollieDev <pieterjanpollie@gmail.com>
2026-04-25 13:01:47 +0000

    chore: sync client version to 0.5.2

--- src/SkribbyClient.ts
+++ src/SkribbyClient.ts
@@ -21,7 +21,7 @@ export type SkribbyClientOptions = {
 };
 
 const SKRIBBY_CLIENT = 'js-sdk';
-const SKRIBBY_CLIENT_VERSION = '0.5.1';
+const SKRIBBY_CLIENT_VERSION = '0.5.2';
 
 export class SkribbyClient {
   private readonly api_url: string;
